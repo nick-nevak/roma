f6ff98b13d73438e4ddff114dff70fbdbefcd5fd
Romka <prostoonado@gmail.com>
2022-11-23 21:33:37 +0300

    Добавил компонент приватного маршрута.

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -111,14 +111,7 @@ export default function MainScreen(props: MainScreenProps): JSX.Element {
         </section>
 
         <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
+          <Logo />
           <div className="copyright">
             <p>© 2019 What to watch Ltd.</p>
           </div>
