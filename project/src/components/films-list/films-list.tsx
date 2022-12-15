231a5014f104070c7b578de185d10793c813b6b9
Romka <prostoonado@gmail.com>
2022-12-15 22:07:01 +0300

    task 2 done

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,13 +1,18 @@
 import { Film } from '../../types/types';
 import { BaseSyntheticEvent, useState } from 'react';
 import FilmCard from '../film-card/film-card';
-// import ShowMore from '../show-more/show-more';
+import ShowMore from '../show-more/show-more';
+import { getFilmsByGenre } from '../../store/films/films-slice';
+import { Genre } from '../../const';
+import { useDispatch } from 'react-redux';
 
 export type FilmsListProps = {
   films: Film[];
 }
 
 export default function FilmsList({ films }: FilmsListProps): JSX.Element {
+  const dispatch = useDispatch();
+  const handleShowMore = () => dispatch(getFilmsByGenre(Genre.AllGenres));
   const [activeId, setActiveId] = useState<string | null>(null);
 
   const handleMouseOver = (evt: BaseSyntheticEvent) => {
@@ -23,6 +28,7 @@ export default function FilmsList({ films }: FilmsListProps): JSX.Element {
   return (
     <div className="catalog__films-list" onMouseOver={handleMouseOver} onMouseOut={() => setActiveId(null)}>
       {films.map((film: Film) => <FilmCard film={film} isActive={film.id.toString() === activeId} key={film.id} />)}
+      <ShowMore onClick={handleShowMore} />
     </div>
   );
 }
